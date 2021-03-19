228ad636eff9d01b6acfec92b9cd9a9a0da9f55e
Bernardo Sertório <besertorio@gmail.com>
2021-03-19 12:54:28 -0300

    refactoring and creating Content component

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from 'react';
 import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
 
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
@@ -12,41 +12,3 @@ import './styles/global.scss';
 
 import './styles/content.scss';
 
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
-
-export function App() {
-  const [movies, setMovies] = useState<MovieProps[]>([]);
-
-  return (
-    <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard 
-                key ={movie.imdbID} 
-                title={movie.Title} 
-                poster={movie.Poster} 
-                runtime={movie.Runtime} 
-                rating={movie.Ratings[0].Value} 
-              />
-            ))}
-          </div>
-        </main>
-      </div>
-    </div>
-  )
-}
